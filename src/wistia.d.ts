888bbb7f77bc64f5e463103d1eaf1dbbb05070b6
Juan Jose <juanjorianozabaleta@gmail.com>
2026-05-06 12:58:58 -0500

    Changes 1.1

--- src/wistia.d.ts
+++ src/wistia.d.ts
@@ -1,8 +1,12 @@
-declare namespace JSX {
-    interface IntrinsicElements {
-        'wistia-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-            'media-id'?: string
-            aspect?: string | number
+import 'react'
+
+declare module 'react' {
+    namespace JSX {
+        interface IntrinsicElements {
+            'wistia-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+                'media-id'?: string
+                aspect?: string | number
+            }
         }
     }
 }
